f6f46d3244b66a6f96fe7ddd57f3953c9535dc01
KD Natividad <kdnatividad00@gmail.com>
2026-02-18 16:30:14 +0800

    added 24px y axis padding on navlinks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 export const Header = () => {
   return (
     <header className="sticky top-0 z-50 w-full bg-[#020611]/80 backdrop-blur-sm">
-      <div className="max-w-3xl mx-auto px-8 h-[84px] flex items-center">
+      <div className="max-w-3xl mx-auto px-8 py-6 h-[84px] flex items-center">
         <Nav />
       </div>
     </header>
