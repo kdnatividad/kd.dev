e468701aeca6b09c4d29bac6c991bea888fbf72a
KD Natividad <kdnatividad00@gmail.com>
2026-03-02 15:30:11 +0800

    added education

---
+++ src/components/career/careerdata.tsx
@@ -0,0 +1,84 @@
+import agwebworx from "../../assets/agwebworx.webp";
+import deped from "../../assets/deped.webp";
+import jnc from "../../assets/jnc.webp";
+import eviva from "../../assets/eviva.webp";
+import wedodigital from "../../assets/wedodigital.webp";
+import Blogtec from "../../assets/Blogtec.webp";
+import spup from "../../assets/spup.webp";
+import sjc from "../../assets/sjc.webp";
+export const workData = [
+  {
+    company: "AG WebWorx",
+    role: "WordPress Developer, Project Manager",
+    date: "March 2020 - Present",
+    logo: agwebworx,
+    bullets: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    ],
+  },
+  {
+    company: "JNC Sports PH",
+    role: "Manager, Creative Director",
+    date: "August 2023 - Nov 2025",
+    logo: jnc,
+    bullets: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    ],
+  },
+  {
+    company: "BlogTec",
+    role: "Content Management Specialist",
+    date: "Nov 2022 - Jul 2023 (Contract)",
+    logo: Blogtec,
+    bullets: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    ],
+  },
+  {
+    company: "Department of Education, Tuguegarao",
+    role: "Developer, Team Leader, Intern  (OJT)",
+    date: "March 2023 - May 2023",
+    logo: deped,
+    bullets: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    ],
+  },
+  {
+    company: "Eviva Media",
+    role: "WordPress Developer  (Part-Time)",
+    date: "March 2021 - 2022",
+    logo: eviva,
+    bullets: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    ],
+  },
+  {
+    company: "We Do Digital (Intern)",
+    role: "WordPress Developer",
+    date: "March 2020 - 2021",
+    logo: wedodigital,
+    bullets: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    ],
+  },
+];
+export const educData = [
+  {
+    alma: "St. Paul University Philippines",
+    logo: spup,
+    major: "Bachelor of Science in Information Technology",
+    date: "2019 - 2023",
+    bullets: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    ],
+  },
+  {
+    alma: "Saint Joseph's College of Baggao",
+    logo: sjc,
+    major: "Science, Technology, Engineering, and Mathematics",
+    date: "2013-2019",
+    bullets: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    ],
+  },
+];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import Hero from "../components/Hero";
-import Work from "../components/Work";
+import Career from "../components/career/Career";
 import Projects from "../components/Projects";
 import Blogs from "../components/Blogs";
 
 const Home = () => {
   return (
-    <main className="flex flex-col gap-24">
+    <main className="flex flex-col gap-8">
       <Hero />
-      <Work />
+      <Career />
       <Projects />
       <Blogs />
     </main>
