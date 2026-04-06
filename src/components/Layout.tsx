3e62a6658f09b7c1eae96a1ccae59b2e05561317
KD Natividad <kdnatividad00@gmail.com>
2026-04-06 09:58:05 +0800

    Layout Tweaks

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,15 @@
 import Header from "./Header";
+import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 import "../index.css";
 export default function Layout() {
   return (
-    <div className="min-h-screen bg-gray-950 text-white font-sans">
-      <Header /> {/* full width, constrains content inside */}
-      <main className="max-w-[770px] mx-auto px-8">
+    <div className="min-h-screen bg-gray-950 text-white font-sans flex flex-col">
+      <Header />
+      <main className="max-w-[770px] mx-auto px-8 w-full flex-1">
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 }
