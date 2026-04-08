fbe9af66d82e0987afebe5b092510d4d40947817
KD Natividad <kdnatividad00@gmail.com>
2026-04-09 06:30:09 +0800

    Added Contact content

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import ProjectsPage from "./pages/Projects";
+import ContactPage from "./pages/Contact";
 
 export default function App() {
   return (
@@ -11,7 +12,7 @@ export default function App() {
         <Route path="/about" element={<div>About</div>} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/blog" element={<div>Blog</div>} />
-        <Route path="/contact" element={<div>Contact</div>} />
+        <Route path="/contact" element={<ContactPage />} />
       </Route>
     </Routes>
   );
